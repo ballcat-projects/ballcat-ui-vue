590d31e15bf17c6c0f36bf47769fb6412347d99b
hccake <chengbohua@foxmail.com>
2020-04-12 16:20:59 +0800

    :art: 修改lsKeyPrefix

--- src/components/SettingDrawer/SettingDrawer.vue
+++ src/components/SettingDrawer/SettingDrawer.vue
@@ -231,7 +231,7 @@ export default {
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true',
   // vue-ls options
   storageOptions: {
-    namespace: 'pro__',
+    namespace: 'ballcat__',
     name: 'ls',
     storage: 'local',
   }
--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -26,7 +26,7 @@ export default {
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true',
   // vue-ls options
   storageOptions: {
-    namespace: 'pro__', // key prefix
+    namespace: 'ballcat__', // key prefix
     name: 'ls', // name variable Vue.[ls] or this.[$ls],
     storage: 'local' // storage name session, local, memory
   }
