001db51d2baba4b6fd0ae1a691ebe535dd769a22
聆听 <lingting@gmail.com>
2020-08-12 20:48:32 +0800

    :bug: 修复select 无法正确获取值

--- src/components/dict/DictSelect.vue
+++ src/components/dict/DictSelect.vue
@@ -1,28 +1,29 @@
 <template>
    <span>
        <a-select
+         :allowClear="true"
          :placeholder="placeholder"
          :disabled="disabled"
          :value="selectedValue"
          @change="handleChange">
         <a-select-option v-if="placeholderOption" value="">{{placeholder}}</a-select-option>
-        <a-select-option v-for="dict in dictItems" :key="Number(dict.value)">
+        <a-select-option v-for="dict in dictItems" :key="getValByItem(dict)">
             {{ dict.name }}
         </a-select-option>
       </a-select>
     </span>
 </template>
 <script>
-  import DictMixin from '@/components/dict/dictMixin'
-  export default {
-    name: 'DictSelect',
-    mixins: [DictMixin],
-    props: {
-      placeholder: String,
-      placeholderOption: {
-        type: Boolean,
-        default: false
-      }
+import DictMixin from '@/components/dict/dictMixin'
+export default {
+  name: 'DictSelect',
+  mixins: [DictMixin],
+  props: {
+    placeholder: String,
+    placeholderOption: {
+      type: Boolean,
+      default: false
     }
   }
+}
 </script>
