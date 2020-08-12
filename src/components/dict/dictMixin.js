0f30b424c427f03202c9d1788a5a6f73ad04473a
聆听 <lingting@gmail.com>
2020-08-12 16:36:40 +0800

    :sparkles: 字典添加数据类型

--- src/components/dict/dictMixin.js
+++ src/components/dict/dictMixin.js
@@ -1,28 +1,28 @@
 export default {
   name: 'DictMixin',
   props: {
-    value: [String, Number],
+    value: [String, Number, Boolean],
     dictCode: String,
-    disabled: Boolean,
+    disabled: Boolean
   },
-  data() {
+  data () {
     return {
       dictItems: {},
       selectedValue: this.value
     }
   },
   watch: {
-    value() {
+    value () {
       this.selectedValue = this.value
     }
   },
-  created() {
+  created () {
     this.DictPool.getDictItems(this.dictCode).then(dictItems => {
       this.dictItems = dictItems
     })
   },
   methods: {
-    handleChange(val) {
+    handleChange (val) {
       if (val.target) {
         this.selectedValue = val.target.value
       } else {
@@ -32,6 +32,23 @@ export default {
       this.$emit('change', this.selectedValue)
       // v-model 方式的表单值联动
       this.$emit('input', this.selectedValue)
+    },
+    getValByItem (dict) {
+      let res = Number(dict.value)
+      if (dict.type) {
+        if (dict.type === 1) {
+          // 数字
+          res = Number(dict.value)
+        } else if (dict.type === 2) {
+          // 字符串
+          res = String(dict.value)
+        } else if (dict.type === 3) {
+          // 布尔
+          res = Boolean(dict.value)
+        }
+      }
+      // 如果没有type， 按number 处理
+      return res
     }
   }
 }
--- src/views/sys/dict/SysDictForm.vue
+++ src/views/sys/dict/SysDictForm.vue
@@ -1,59 +1,54 @@
 <template>
   <a-form @submit="handleSubmit" :form="form">
-
     <a-form-item v-if="formAction === this.FORM_ACTION.UPDATE" style="display: none">
       <a-input v-decorator="['id']"/>
     </a-form-item>
 
-    <a-form-item label="标识"
-                 :labelCol=labelCol
-                 :wrapperCol=wrapperCol>
-      <a-input placeholder="字典标识"
-               :disabled="formAction === this.FORM_ACTION.UPDATE"
-               v-decorator="['code']"/>
+    <a-form-item label="标识" :labelCol="labelCol" :wrapperCol="wrapperCol">
+      <a-input
+        placeholder="字典标识"
+        :disabled="formAction === this.FORM_ACTION.UPDATE"
+        v-decorator="['code']"
+      />
     </a-form-item>
 
-    <a-form-item label="名称"
-                 :labelCol=labelCol
-                 :wrapperCol=wrapperCol>
-      <a-input placeholder="字典名称"
-               v-decorator="['title']"/>
+    <a-form-item label="名称" :labelCol="labelCol" :wrapperCol="wrapperCol">
+      <a-input placeholder="字典名称" v-decorator="['title']"/>
     </a-form-item>
 
-    <a-form-item label="字典类型"
-                 :labelCol=labelCol
-                 :wrapperCol=wrapperCol>
-      <dict-select placeholder="字典类型"
-                   v-decorator="['editable']"
-                   dict-code="dict_property">
-      </dict-select>
+    <a-form-item label="字典类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
+      <dict-radio-group
+        placeholder="字典类型"
+        v-decorator="['editable',decoratorOptions.editable]"
+        dict-code="dict_property"
+      />
     </a-form-item>
 
-    <a-form-item label="备注"
-                 :labelCol=labelCol
-                 :wrapperCol=wrapperCol>
-      <a-textarea placeholder="备注"
-                  v-decorator="['remarks']"
-                  :autoSize="{ minRows: 3, maxRows: 5 }"/>
+    <a-form-item label="数据类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
+      <dict-radio-group
+        placeholder="数据类型"
+        v-decorator="['type',decoratorOptions.type]"
+        dict-code="dict_type"
+      />
+    </a-form-item>
+
+    <a-form-item label="备注" :labelCol="labelCol" :wrapperCol="wrapperCol">
+      <a-textarea
+        placeholder="备注"
+        v-decorator="['remarks']"
+        :autoSize="{ minRows: 3, maxRows: 5 }"
+      />
     </a-form-item>
 
     <div v-show="formAction === this.FORM_ACTION.UPDATE">
-      <a-form-item
-        label="创建时间"
-        :labelCol=labelCol
-        :wrapperCol=wrapperCol>
+      <a-form-item label="创建时间" :labelCol="labelCol" :wrapperCol="wrapperCol">
         <span>{{ displayData.createTime }}</span>
       </a-form-item>
-      <a-form-item
-        label="修改时间"
-        :labelCol=labelCol
-        :wrapperCol=wrapperCol>
+      <a-form-item label="修改时间" :labelCol="labelCol" :wrapperCol="wrapperCol">
         <span>{{ displayData.updateTime }}</span>
       </a-form-item>
     </div>
-    <a-form-item
-      :wrapperCol="{offset: 3 }"
-    >
+    <a-form-item :wrapperCol="{offset: 3 }">
       <a-button htmlType="submit" type="primary" :loading="submitLoading">提交</a-button>
       <a-button style="margin-left: 8px" @click="backToPage(false)">取消</a-button>
     </a-form-item>
@@ -71,17 +66,24 @@ export default {
   mixins: [FormPageMixin],
   data () {
     return {
+      dictCodes: ['dict_type'],
       reqFunctions: {
         create: addObj,
         update: putObj
       },
 
       // 校验配置
-      decoratorOptions: {}
+      decoratorOptions: {
+        editable: {
+          initialValue: 1
+        },
+        type: {
+          initialValue: 1
+        }
+      }
     }
   },
   methods: {}
 }
 </script>
-
 
