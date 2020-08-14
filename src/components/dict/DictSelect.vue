f0fa9c2395212300465c4e05537e6165a35a0c8a
聆听 <lingting@gmail.com>
2020-08-14 17:00:16 +0800

    :bug: 修复 dict-select 在有些位置上宽度异常

--- src/components/dict/DictSelect.vue
+++ src/components/dict/DictSelect.vue
@@ -1,20 +1,20 @@
 <template>
-   <span>
-       <a-select
-         :allowClear="true"
-         :placeholder="placeholder"
-         :disabled="disabled"
-         :value="selectedValue"
-         @change="handleChange">
-        <a-select-option v-if="placeholderOption" value="">{{placeholder}}</a-select-option>
-        <a-select-option v-for="dict in dictItems" :key="getValByItem(dict)">
-            {{ dict.name }}
-        </a-select-option>
-      </a-select>
-    </span>
+  <a-select
+    :allowClear="true"
+    :placeholder="placeholder"
+    :disabled="disabled"
+    :value="selectedValue"
+    style="width: 100%;"
+    @change="handleChange">
+    <a-select-option v-if="placeholderOption" value="">{{ placeholder }}</a-select-option>
+    <a-select-option v-for="dict in dictItems" :key="getValByItem(dict)">
+      {{ dict.name }}
+    </a-select-option>
+  </a-select>
 </template>
 <script>
 import DictMixin from '@/components/dict/dictMixin'
+
 export default {
   name: 'DictSelect',
   mixins: [DictMixin],
