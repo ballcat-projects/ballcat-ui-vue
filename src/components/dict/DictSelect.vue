e43906dd35df94403f94efa5516cac2804cdfbc5
聆听 <lingting@gmail.com>
2020-08-19 10:56:00 +0800

    :bug: 修复 dictSlot和dictText值展示异常

--- src/components/dict/DictSelect.vue
+++ src/components/dict/DictSelect.vue
@@ -7,8 +7,8 @@
     style="width: 100%;"
     @change="handleChange">
     <a-select-option v-if="placeholderOption" value="">{{ placeholder }}</a-select-option>
-    <a-select-option v-for="dict in dictItems" :key="getValByItem(dict)">
-      {{ dict.name }}
+    <a-select-option v-for="dict in dictItems" :key="dict.id" :value="getValByItem(dict)">
+      {{dict.name}}
     </a-select-option>
   </a-select>
 </template>
--- src/components/dict/DictText.vue
+++ src/components/dict/DictText.vue
@@ -1,25 +1,43 @@
 <template>
-  <span>{{dictItem? dictItem.name: value}}</span>
+  <span>{{ dictItem ? dictItem.name : value }}</span>
 </template>
 <script>
-  export default {
-    name: 'DictText',
-    props: {
-      value: [Number, String],
-      dictCode: String,
-      color: String
-    },
-    data() {
-      return {
-        dictItem: null,
+export default {
+  name: 'DictText',
+  props: {
+    value: [Number, String, Boolean],
+    dictCode: String,
+    color: String
+  },
+  data () {
+    return {
+      dictItem: null
+    }
+  },
+  mounted () {
+    let dictData = this.DictPool.dictDataCache[this.dictCode]
+    if (dictData) {
+      this.dictItem = dictData.find(dictItem => this.getValByItem(dictItem) === this.value)
+    }
+  },
+  methods: {
+    getValByItem (dict) {
+      let res = String(dict.value)
+      if (dict.valueType) {
+        if (dict.valueType === 1) {
+          // 数字
+          res = Number(dict.value)
+        } else if (dict.valueType === 2) {
+          // 字符串
+          res = String(dict.value)
+        } else if (dict.valueType === 3) {
+          // 布尔
+          res = Boolean(dict.value)
+        }
       }
-    },
-    mounted() {
-      let dictData = this.DictPool.dictDataCache[this.dictCode]
-      if(dictData){
-        this.dictItem = dictData.find(dictItem => dictItem.value === this.value.toString())
-      }
-    },
-    methods: {}
+      // 如果没有type， 按 String 处理
+      return res
+    }
   }
+}
 </script>
