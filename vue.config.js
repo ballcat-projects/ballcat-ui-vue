52f2f1d58e54c7817b9d5ea6d1804bad19eedc99
baixiaoxu <baixx2@guahao.com>
2020-08-30 13:14:32 +0800

    feat:重置vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const createThemeColorReplacerPlugin = require('./config/plugin.config')
-const url = 'http://preview.ballcat.cn'
+const url = 'http://ballcat-admin:8080'
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -96,22 +96,14 @@ const vueConfig = {
     port: 8000,
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
     proxy: {
-      '^/api/sys1': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-          '^/api/sys1': '/api',
-        }
-      },
       '^/api': {
         target: url,
         changeOrigin: true,
         ws: true,
         pathRewrite: {
-          '^/api': '/api',
+          '^/api': '/'
         }
-      },
+      }
     }
   },
 
