7216bf62201c7d711e5c0aa5f7406480a8b65280
hccake <chengbohua@foxmail.com>
2020-09-05 00:27:19 +0800

    :art: 代码结构微微调

--- src/components/dict/dictItemMixin.js
+++ src/components/dict/dictItemMixin.js
@@ -1,4 +1,4 @@
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'DictItemMixin',
@@ -6,39 +6,35 @@ export default {
     ...mapState({
       dictDataCache: state => state.dict.dictDataCache
     }),
-    dictItems() {
+    dictItems () {
       if (!this.dictDataCache[this.dictCode]) {
-        this.requestData();
-        return [];
+        this.requestData()
+        return []
       }
-      return this.dictDataCache[this.dictCode];
+      return this.dictDataCache[this.dictCode]
     }
   },
   created () {
     if (!this.dictDataCache[this.dictCode]) {
-      this.requestData();
+      this.requestData()
     }
   },
   methods: {
-    requestData() {
-      this.getDictData([this.dictCode]);
+    requestData () {
+      this.getDictData([this.dictCode])
     },
     ...mapActions(['getDictData']),
     getValByItem (dict) {
-      let res = Number(dict.value)
-      if (dict.valueType) {
-        if (dict.valueType === 1) {
-          // 数字
-          res = Number(dict.value)
-        } else if (dict.valueType === 2) {
-          // 字符串
-          res = String(dict.value)
-        } else if (dict.valueType === 3) {
-          // 布尔
-          res = Boolean(dict.value)
-        }
-      }
+      let res = dict.value
       // 如果没有type， 按number 处理
+      let valueType = dict.valueType || 1
+      if (valueType === 1) {
+        res = Number(dict.value)         // 数字
+      } else if (valueType === 2) {
+        res = String(dict.value)         // 字符串
+      } else if (valueType === 3) {
+        res = Boolean(dict.value)        // 布尔
+      }
       return res
     }
   }
