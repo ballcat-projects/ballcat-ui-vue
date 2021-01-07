ab219d6e5d49d505659b64c1c14ff4e133d8eae9
hccake <chengbohua@foxmail.com>
2021-01-07 17:26:41 +0800

    :ambulance: simple commit

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,8 +1,10 @@
 import * as CryptoJS from 'crypto-js'
 
+const securityKey = '==BallCat-Auth=='
+
 export const encryption = (pass) => {
   // 密码加密
-  const key = CryptoJS.enc.Latin1.parse('==BallCat-Auth==')
+  const key = CryptoJS.enc.Latin1.parse(securityKey)
   return CryptoJS.AES.encrypt(
     pass,
     key, {
