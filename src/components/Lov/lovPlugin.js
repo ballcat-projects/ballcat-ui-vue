4fe9e724cca34fe8523c5fbd6da67d6be63a8dd1
dixiaoping <dixiaoping@moppomobi.com>
2021-06-09 11:00:18 +0800

    lov 本地化储存

--- src/components/Lov/lovPlugin.js
+++ src/components/Lov/lovPlugin.js
@@ -1,7 +1,8 @@
 import Lov from '@/components/Lov/Lov'
-
+import LovLocal from '@/components/Lov/LovLocal'
 export default {
   install: function (Vue) {
     Vue.component('Lov', Lov)
+    Vue.component('LovLocal', LovLocal)
   }
 }
