b10b904c3b24314611e620221f8e4edda3556e59
lingting <sunlisten.gzm@gmail.com>
2021-10-27 15:39:02 +0800

    feat: 添加对 DictBadge 的支持

---
+++ src/components/Dict/display/DictBadge.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-badge :status="status" :color="color" :text="showText" />
+</template>
+<script>
+import DictDisplayMixin from '@/components/Dict/display/dictDisplayMixin'
+
+export default {
+  name: 'DictBadge',
+  mixins: [DictDisplayMixin],
+  computed: {
+    color() {
+      let color
+      if (this.dictItem.attributes) {
+        color = this.dictItem.attributes['badgeColor']
+      }
+      return this.colors[this.value] || color || this.uniformColor
+    },
+    status() {
+      let status
+
+      if (this.dictItem.attributes) {
+        status = this.dictItem.attributes['badgeStatus']
+      }
+      return status
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
