1804968b13ed3f2e781b51ff2f1d6339354cbbc2
Hccake <chengbohua@foxmail.com>
2021-12-06 20:07:41 +0800

    :fire: 移除过时的 lov 相关代码，推荐使用 lov-local 组件

--- src/components/Lov/lovPlugin.js
+++ src/components/Lov/lovPlugin.js
@@ -1,8 +1,7 @@
-import Lov from '@/components/Lov/Lov'
 import LovLocal from '@/components/Lov/LovLocal'
+
 export default {
   install: function (Vue) {
-    Vue.component('Lov', Lov)
     Vue.component('LovLocal', LovLocal)
   }
 }
