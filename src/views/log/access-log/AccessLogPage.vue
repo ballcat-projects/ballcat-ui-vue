61ec7502d61e03d17733ac389308504912f544e6
ace <oncwnuEpZAyPF2Ue7VvaNcMx6xS4@git.weixin.qq.com>
2022-01-23 20:13:05 +0800

    :bug: 日志查询重置无效

--- src/views/log/access-log/AccessLogPage.vue
+++ src/views/log/access-log/AccessLogPage.vue
@@ -52,7 +52,7 @@
               <a-space size="middle">
                 <a-space>
                   <a-button type="primary" :loading="searchFormState.loading" @click="searchFormState.reloadTable(true)">查询</a-button>
-                  <a-button @click="searchFormState.resetSearchForm && resetSearchTime()">重置</a-button>
+                  <a-button @click="resetSearchTime() && searchFormState.resetSearchForm()">重置</a-button>
                 </a-space>
                 <a @click="searchFormState.toggleSearchCollapsed">
                   {{ searchFormState.collapsed ? '展开': '收起' }}
